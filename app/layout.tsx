33e2d0dbed8d858024ef8f5f52d33009eb003ad7
hktari <bkamnik1995@gmail.com>
2024-08-09 09:10:16 +0200

    install sass and configure bootstrap import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import "../styles.css";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../styles/global.scss";
+
 
 import React from "react";
 import { ThemeProvider } from "../components/theme-provider";
