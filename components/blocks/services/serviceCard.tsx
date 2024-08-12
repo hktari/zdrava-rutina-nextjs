fa758bff9c8db4f3767802cb61f6cda3abfd004b
hktari <bkamnik1995@gmail.com>
2024-08-12 12:07:34 +0200

    add data-tina-field attributes to service cards

--- components/blocks/services/serviceCard.tsx
+++ components/blocks/services/serviceCard.tsx
@@ -2,18 +2,20 @@ import React from "react";
 import Image from "next/image";
 import { PageBlocksServicesItems } from "../../../tina/__generated__/types";
 import Link from "next/link";
+import { tinaField } from "tinacms/dist/react";
 
-const ServiceCard = ({
-  link,
-  description,
-  image,
-  title,
-}: PageBlocksServicesItems) => {
+const ServiceCard = (serviceCardData: PageBlocksServicesItems) => {
+  const { link, description, image, title } = serviceCardData;
+
   return (
     <Link href={link}>
       <div className="c-service-card card border-0 border-none p-0">
         {image && (
-          <div className="card-img-top position-relative" style={{ overflow: "hidden" }}>
+          <div
+            data-tina-field={tinaField(serviceCardData, "image")}
+            className="card-img-top position-relative"
+            style={{ overflow: "hidden" }}
+          >
             <Image
               fill
               sizes="(max-width: 768px) 100vw, 50vw"
@@ -25,8 +27,18 @@ const ServiceCard = ({
           </div>
         )}
         <div className="card-body">
-          <h5 className="c-service-card__title card-title">{title}</h5>
-          <p className="card-text opacity-75">{description}</p>
+          <h5
+            data-tina-field={tinaField(serviceCardData, "title")}
+            className="c-service-card__title card-title"
+          >
+            {title}
+          </h5>
+          <p
+            className="card-text opacity-75"
+            data-tina-field={tinaField(serviceCardData, "description")}
+          >
+            {description}
+          </p>
         </div>
       </div>
     </Link>
