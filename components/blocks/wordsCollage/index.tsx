60a95c571ab0096c95d9afd9da9a61a55abc97c6
hktari <bkamnik1995@gmail.com>
2024-08-12 12:18:47 +0200

    add defaultItem to WordCollageBlock schema and data-tina-field attribute

--- components/blocks/wordsCollage/index.tsx
+++ components/blocks/wordsCollage/index.tsx
@@ -1,21 +1,27 @@
 import React, { useRef } from "react";
 import { useInView } from "framer-motion";
 import { Section } from "../../layout/section";
+import { tinaField } from "tinacms/dist/react";
+import { PageBlocksWordCollageWords } from "../../../tina/__generated__/types";
+
+type WordCollageItemProps = {
+  word: PageBlocksWordCollageWords;
+  initialTranslateX: number | string;
+  idx: number;
+};
 
 const WordCollageItem = ({
-  text,
-  fontSize,
-  xOffset,
-  fontStyle,
-  fontWeight,
+  word,
   initialTranslateX = 0,
   idx,
-}) => {
+}: WordCollageItemProps) => {
+  const { text, fontSize, xOffset, fontStyle, fontWeight } = word;
   const ref = useRef();
   const isInView = useInView(ref, { once: true });
 
   return (
     <span
+      data-tina-field={tinaField(word, "text")}
       ref={ref}
       className={`c-word-collage--item text-center fs-${fontSize} `}
       style={{
@@ -43,7 +49,7 @@ const WordCollageBlock = ({ data }) => {
           <WordCollageItem
             key={`${word}-${idx}`}
             idx={idx}
-            {...word}
+            word={word}
             initialTranslateX={
               idx % 2 === 0 ? "clamp(-50vw, 500px)" : "clamp(50vw, 500px)"
             }
@@ -56,19 +62,69 @@ const WordCollageBlock = ({ data }) => {
 
 export default WordCollageBlock;
 
-const defaultWordCollageItem = {};
+const defaultWordCollageItem = {
+  text: "slabše počutje",
+  fontSize: "2",
+  xOffset: 5,
+  fontStyle: "italic",
+  fontWeight: "600",
+};
+
+const defaultWordCollageItem2 = {
+  text: "pomanjkanje energije",
+  fontSize: "5",
+  xOffset: -25,
+  fontStyle: "normal",
+  fontWeight: "400",
+};
+
+const defaultWordCollageItem3 = {
+  text: "nezadovoljstvo",
+  fontSize: "1",
+  xOffset: 35,
+  fontStyle: "italic",
+  fontWeight: "400",
+};
+
+const defaultWordCollageItem4 = {
+  text: "nevoščljivost",
+  fontSize: "1",
+  xOffset: -15,
+  fontStyle: "normal",
+  fontWeight: "400",
+};
+
+const defaultWordCollageItem5 = {
+  text: "odtujenost",
+  fontSize: "3",
+  xOffset: 5,
+  fontStyle: "italic",
+  fontWeight: "600",
+};
+
+const defaultWordCollageItem6 = {
+  text: "agresija",
+  fontSize: "2",
+  xOffset: -15,
+  fontStyle: "normal",
+  fontWeight: "400",
+};
+
+export const defaultWordCollageItems = [
+  defaultWordCollageItem,
+  defaultWordCollageItem2,
+  defaultWordCollageItem3,
+  defaultWordCollageItem4,
+  defaultWordCollageItem5,
+  defaultWordCollageItem6,
+];
 
 export const wordCollageBlockSchema: import("tinacms").Template = {
   name: "wordCollage",
   label: "Kolaž Besed",
   ui: {
-    previewSrc: "",
     defaultItem: {
-      items: [
-        defaultWordCollageItem,
-        defaultWordCollageItem,
-        defaultWordCollageItem,
-      ],
+      words: [...defaultWordCollageItems],
     },
   },
   fields: [
--- content/pages/home.md
+++ content/pages/home.md
@@ -1,6 +1,38 @@
 ---
 title: Zdrava Rutina
 blocks:
+  - words:
+      - text: slabše počutje
+        fontSize: '2'
+        xOffset: 5
+        fontStyle: italic
+        fontWeight: '600'
+      - text: pomanjkanje energije
+        fontSize: '5'
+        xOffset: -25
+        fontStyle: normal
+        fontWeight: '400'
+      - text: nezadovoljstvo
+        fontSize: '1'
+        xOffset: 35
+        fontStyle: italic
+        fontWeight: '400'
+      - text: nevoščljivost
+        fontSize: '1'
+        xOffset: -15
+        fontStyle: normal
+        fontWeight: '400'
+      - text: odtujenost
+        fontSize: '3'
+        xOffset: 5
+        fontStyle: italic
+        fontWeight: '600'
+      - text: agresija
+        fontSize: '2'
+        xOffset: -15
+        fontStyle: normal
+        fontWeight: '400'
+    _template: wordCollage
   - title: Kontaktirajte nas
     contactItems:
       email: hi@zdrava-rutina.com
