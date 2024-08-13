04712d4b4d1c50d4ccefb31833963fc3c41808d1
hktari <bkamnik1995@gmail.com>
2024-08-13 12:48:15 +0200

    implement responsive masonry component and PhotoAlbumItem component with next/image

--- components/blocks/gallery/index.tsx
+++ components/blocks/gallery/index.tsx
@@ -2,31 +2,33 @@ import React, { useCallback, useState } from "react";
 import PhotoAlbumItem from "./photoAlbumItem";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import { Template } from "tinacms";
+import { PageBlocksImageGallery } from "../../../tina/__generated__/types";
+import Section from "../../layout/section";
 
-export type PhotoAlbumProps = {
-  data: {
-    images: any[];
-  };
+export type GalleryBlockProps = {
+  data: PageBlocksImageGallery;
 };
 
-const GalleryBlock = ({ data }: PhotoAlbumProps) => {
+const GalleryBlock = ({ data }: GalleryBlockProps) => {
   const images = data.images;
   if (!images) {
     return null;
   }
 
   return (
-    <div>
-      <pre>{JSON.stringify(images, null, 2)}</pre>
-
-      {/* <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
+    <Section>
+      <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
         <Masonry>
           {images.map((image) => (
-            <PhotoAlbumItem key={photo.id} photo={photo} minPhotoWidth={432} />
+            <PhotoAlbumItem
+              key={image?.src}
+              image={image}
+              minPhotoWidth={432}
+            />
           ))}
         </Masonry>
-      </ResponsiveMasonry> */}
-    </div>
+      </ResponsiveMasonry>
+    </Section>
   );
 };
 
--- components/blocks/gallery/photoAlbumItem/index.tsx
+++ components/blocks/gallery/photoAlbumItem/index.tsx
@@ -1,35 +1,25 @@
 import React from "react";
+import { PageBlocksImageGalleryImages } from "../../../../tina/__generated__/types";
+import Image from "next/image";
 
 type Props = {
-  photo: PhotosDatum;
+  image: PageBlocksImageGalleryImages;
   minPhotoWidth: number;
 };
 
-const PhotoAlbumItem = ({ photo, minPhotoWidth }: Props) => {
-  const sortByWidthAscending = (photos: WebpImage[]) => {
-    const copy = [...photos];
-    return copy.sort((a, b) => a.width - b.width);
-  };
-
-
-  const takeLargestPhoto = (photos: WebpImage[]) => {
-    const sortedPhotos = sortByWidthAscending(photos);
-    return sortedPhotos[sortedPhotos.length - 1];
-  };
-
-  const largestPhoto = takeLargestPhoto(photo.webp_images);
-  const optimalSizePhoto = takePhotoWithAtLeast(
-    minPhotoWidth,
-    photo.webp_images
-  );
+const PhotoAlbumItem = ({ image, minPhotoWidth }: Props) => {
+  if (!image) {
+    return null;
+  }
 
   return (
     <div className="m-1 rounded-1 " style={{ overflow: "hidden" }}>
-      <a href={largestPhoto.source}>
-        <img
-          className="img-fluid"
-          src={optimalSizePhoto.source}
-          alt={photo?.id}
+      <a href={image.src}>
+        <Image
+          src={image.src}
+          alt={image.alt}
+          width={minPhotoWidth}
+          height={minPhotoWidth}
         />
       </a>
     </div>
