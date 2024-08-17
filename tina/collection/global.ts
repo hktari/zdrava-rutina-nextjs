c0fc5289683d9506bf363262e5571c80608a1e23
hktari <bkamnik1995@gmail.com>
2024-08-17 19:11:45 +0200

    chore: Remove unused icon field from Global collection

--- tina/collection/global.ts
+++ tina/collection/global.ts
@@ -1,6 +1,5 @@
 import type { Collection } from "tinacms";
 import { ColorPickerInput } from "../fields/color";
-import { iconSchema } from "../fields/icon";
 
 const Global: Collection = {
   label: "Global",
@@ -17,21 +16,6 @@ const Global: Collection = {
       name: "header",
       required: true,
       fields: [
-        iconSchema as any,
-        {
-          type: "string",
-          label: "Name",
-          name: "name",
-        },
-        {
-          type: "string",
-          label: "Color",
-          name: "color",
-          options: [
-            { label: "Default", value: "default" },
-            { label: "Primary", value: "primary" },
-          ],
-        },
         {
           type: "object",
           label: "Nav Links",
@@ -84,13 +68,13 @@ const Global: Collection = {
       type: "object",
       label: "Theme",
       name: "theme",
-      // @ts-ignore
       fields: [
         {
           type: "string",
           label: "Primary Color",
           name: "color",
           ui: {
+            // @ts-ignore
             component: ColorPickerInput,
           },
         },
